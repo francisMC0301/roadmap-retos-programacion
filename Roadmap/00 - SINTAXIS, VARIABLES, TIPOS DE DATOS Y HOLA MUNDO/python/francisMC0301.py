9aefb1eec62630747ecc17edfad5da9b57084200
francisMC0301 <franmerino777@gmail.com>
2026-08-16 00:12:11 -0500

    Create francisMC0301.py

---
+++ Roadmap/00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/python/francisMC0301.py
@@ -0,0 +1,13 @@
+# https://www.python.org/
+
+#Comentario de una linea
+
+¨¨
+Existen comentarios de una linea o de varias lineas
+este por ejemplo es de varias lineas
+¨¨
+
+my_variable = ¨esta es una variable¨
+my_variable = ¨puedes hacer varias¨
+
+MY_CONSTANT = ¨esta es una constante, es constante porque no cambia¨
